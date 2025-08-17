3aec7f9b7579d02c922021ceed26579bc3b14146
Ali Mehdizadeh <alimehdizadeh127@gmail.com>
2025-08-17 10:53:14 +0330

    feat: Add initial restaurant models and migrations.

---
+++ restaurant/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+class MenuItem(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='items')
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+
+    
