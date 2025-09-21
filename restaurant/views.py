ad06011259896d45b8a8909494d404256658e5e8
Ali Mehdizadeh <alimehdizadeh127@gmail.com>
2025-09-21 10:20:23 +0330

    Fix: fixed hyperlinks in header, footer and index page to proper pages.

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Menu, MenuItem
@@ -9,13 +11,35 @@ def home_page(request):
     return render(request, 'index.html')
 
 def login_page(request):
-    return render(request, 'login.html', {'form_type': 'login'})
+    """Handle login form display and submission."""
+    if request.method == 'POST':
+        form_login = AuthenticationForm(request=request, data=request.POST)
+        if form_login.is_valid():
+            login(request, form_login.get_user())
+            return redirect('home')
+        
+    form_login = AuthenticationForm()
+    form_register = UserCreationForm()
+    return render(request, "login.html", {"form_login": form_login, "form_register": form_register})
 
-def logout(request):
-    return render(request, 'index.html')
 
 def register_page(request):
-    return render(request, 'login.html', {'form_type': 'register'})
+    """Handle registration form display and submission."""
+    if request.method == "POST":
+        form_register = UserCreationForm(request.POST)
+        if form_register.is_valid():
+            user = form_register.save()
+            login(request, user)
+            return redirect('home')
+    form_register = UserCreationForm()
+    form_login = AuthenticationForm()
+    
+    return render(request, "login.html", {"form_login": form_login, "form_register": form_register})
+    
+    
+def logout(request):
+    logout(request=request)
+    return redirect('home')
 
 def reservations_page(request):
     return render(request, 'table.html')
